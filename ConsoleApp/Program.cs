8a7648b4c97ca7c48450ba51ea89a04d937b633b
Mika Rinkinen <mirinkinen@gmail.com>
2024-01-27 13:25:26 +0200

    Add console application

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ConsoleApp;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var enumerable = GetEnumerable();
+    
+        Assert.IsNull(enumerable);
+    }
+    
+    private static IEnumerable<string> GetEnumerable()
+    {
+        return Enumerable.Empty<string>();
+    }
+}
