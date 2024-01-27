3b74260b755bc4ea15c5302098b0729596118df8
Mika Rinkinen <mirinkinen@gmail.com>
2024-01-27 12:03:14 +0200

    Clean example

---
+++ UnitTests/UnitTests.cs
@@ -0,0 +1,28 @@
+﻿namespace UnitTests;
+
+[TestClass]
+public class UnitTests
+{
+    [TestMethod]
+    public void BreakOnException()
+    {
+        var enumerable = GetEnumerable().GetAwaiter().GetResult();
+
+        Assert.IsNull(enumerable);
+    }
+    
+    [TestMethod]
+    public async Task BreakOnExceptionAsync()
+    {
+        var enumerable = await GetEnumerable();
+
+        Assert.IsNull(enumerable);
+    }
+
+    private static Task<IEnumerable<string>> GetEnumerable()
+    {
+        var emptyEnumeration = Enumerable.Empty<string>();
+        
+        return Task.FromResult(emptyEnumeration);
+    }
+}
