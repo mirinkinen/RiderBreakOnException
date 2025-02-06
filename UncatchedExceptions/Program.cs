ac211f5b05e8a6bde34646f35fc3512d036e5806
Mika Rinkinen <mirinkinen@gmail.com>
2025-02-06 22:10:46 +0200

    Add examples

--- UncatchedExceptions/Program.cs
+++ UncatchedExceptions/Program.cs
@@ -14,20 +14,31 @@ public class Program
         //ExceptionThrownInThirdPartyCode();
         //await ExceptionThrownInMyCodeAsync();
         //await ExceptionThrownInThirdPartyCodeAsync();
-        //ExceptionThrownFromIndexOperator();
-        //ExceptionThrownFromEnumerableFirst();
+        //ExceptionThrownFromListIndexOperator();
+        //ExceptionThrownFromListEnumerableFirst();
+        //ExceptionThrownFromArrayIndexOperator();
+        //ExceptionThrownFromArrayEnumerableFirst();
     }
 
+    /// <summary>
+    /// Rider always stops on exception.
+    /// </summary>
     public static void ExceptionThrownInMyCode()
     {
         throw new Exception("Test");
     }
 
+    /// <summary>
+    /// Rider always stops on exception.
+    /// </summary>
     public static void ExceptionThrownInThirdPartyCode()
     {
         Assert.IsNotNull(null);
     }
 
+    /// <summary>
+    /// Rider always stops on exception.
+    /// </summary>
     public static async Task ExceptionThrownInMyCodeAsync()
     {
         await Task.Delay(10);
@@ -35,6 +46,9 @@ public class Program
         throw new Exception("Test");
     }
 
+    /// <summary>
+    /// Rider always stops on exception.
+    /// </summary>
     public static async Task ExceptionThrownInThirdPartyCodeAsync()
     {
         await Task.Delay(10);
@@ -42,15 +56,39 @@ public class Program
         Assert.IsNotNull(null);
     }
 
-    public static void ExceptionThrownFromIndexOperator()
+    /// <summary>
+    /// Rider always stops on exception.
+    /// </summary>
+    public static void ExceptionThrownFromListIndexOperator()
     {
-        var list = new List<string>();
+        var list = new List<int>();
         var item = list[0];
     }
 
-    public static void ExceptionThrownFromEnumerableFirst()
+    /// <summary>
+    /// Rider always stops on exception.
+    /// </summary>
+    public static void ExceptionThrownFromListEnumerableFirst()
     {
-        var list = new List<string>();
+        var list = new List<int>();
         var item = list.First();
+    }
+    
+    /// <summary>
+    /// Rider always stops on exception.
+    /// </summary>
+    public static void ExceptionThrownFromArrayIndexOperator()
+    {
+        var array = Array.Empty<int>();
+        var item = array[0];
+    }
+
+    /// <summary>
+    /// Rider always stops on exception.
+    /// </summary>
+    public static void ExceptionThrownFromArrayEnumerableFirst()
+    {
+        var array = Array.Empty<int>();
+        var item = array.First();
     }
 }
